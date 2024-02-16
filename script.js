ce2d50a00673c0210f56cebca2c8207c3ad2a6f0
Dhruv Patel <122099280+dhruvurgr8@users.noreply.github.com>
2024-02-16 13:26:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,7 +9,29 @@ function convertToRoman(num) {
       6:['I', 1]
     };
 
-  //your code here
+    let result = '';
+
+    for (let key in obj) {
+        const [symbol, value] = obj[key];
+        
+        // Repeat the symbol while the number is greater than or equal to its value
+        while (num >= value) {
+            result += symbol;
+            num -= value;
+        }
+
+        
+        const nextIndex = parseInt(key) + 1;
+        if (nextIndex in obj) {
+            const [nextSymbol, nextValue] = obj[nextIndex];
+            if (value - nextValue <= num && num < value) {
+                result += nextSymbol + symbol;
+                num -= (value - nextValue);
+            }
+        }
+    }
+
+    return result;
 
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
